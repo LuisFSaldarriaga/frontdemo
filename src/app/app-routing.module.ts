7239e9ca8142c19cfa0e7827db285999c30c3086
LuisFSaldarriaga <lufesaca@hotmail.com>
2022-03-20 18:51:12 -0500

    v0.1 (routing)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { consultHacedor } from './components/consults/consultHacedor.component';
+import { registerCliente } from './components/register/registerCliente.component';
+import { registerHacedor } from './components/register/registerHacedor.component';
+import { serviceSolicitar } from './components/services/serviceSolicitar.component';
+import { serviceSolicitarTipo } from './components/services/serviceSolicitarTipo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'register/hacedor',
+  component:registerHacedor
+},
+{
+  path:'register/cliente',
+  component:registerCliente
+},
+{
+  path:'services/solicitar',
+  component:serviceSolicitar
+},
+{
+  path:'services/solicitar_tipo',
+  component:serviceSolicitarTipo
+},
+{
+  path:'consult/hacedor',
+  component:consultHacedor
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
